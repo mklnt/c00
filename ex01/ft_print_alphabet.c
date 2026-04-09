7f23c7a258b64388df7fc2857ada051b1fde37e9
mklnt <micmarlen@gmail.com>
2026-04-09 02:10:20 +0200

    ex01

---
+++ ex01/ft_print_alphabet.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+void	ft_print_alphabet(void);
+
+int	main(void)
+{
+	ft_print_alphabet();
+}
+
+void	ft_print_alphabet(void)
+{
+	write(1, "abcdefghijklmnopqrstuwxyz", 26);
+}
